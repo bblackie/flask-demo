58ece48a90e0d1331b3a8b264647601cd1407110
bblackie <brian.blackie@trinityschools.nz>
2026-04-14 09:09:22 +1200

    Creator updates

--- app.py
+++ app.py
@@ -1,18 +1,51 @@
 # app.py
-'''
-Import at the top of the file to avoid circular imports
-'''
-from flask import Flask
-from flask import g
+from flask import Flask, g, render_template
 import sqlite3
+
+DATABASE = 'database.db'
 
 app = Flask(__name__)
 
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
-@app.route("/")
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+@app.route('/')
 def home():
+    #home page- just the ID, Maker, Model and Image URL
+    sql = """
+    SELECT Bikes.BikeID, Makers.Name, Bikes.Model, Bikes.ImageURL 
+    FROM Bikes
+    JOIN Makers ON Makers.MakerID = Bikes.MakerID;
+    """
+    results = query_db(sql)
+    return render_template('home.html', title='Home', bikes=results)
 
+@app.route("/bike/<int:id>")
+def bike(id):
+    #just one bike based on the id
+    sql = """
+    SELECT * FROM Bikes
+    JOIN Makers ON Makers.MakerID = Bikes.MakerID 
+    WHERE Bikes.BikeID = ?;
+    """
+    result = query_db(sql, (id,), True)
+    return render_template('bike.html', title='Bike Details', bike=result)
 
 
 
